a544d5a2aa116148dcf72a5f5335ac05f39e414b
snakerflow <snakerflow@163.com>
2014-02-27 22:46:32 +0800

    移除processName的获取

--- src/main/java/org/snaker/engine/entity/HistoryOrder.java
+++ src/main/java/org/snaker/engine/entity/HistoryOrder.java
@@ -17,7 +17,6 @@ package org.snaker.engine.entity;
 import java.io.Serializable;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
-import org.snaker.engine.core.ModelContainer;
 
 /**
  * 历史流程实例实体类
@@ -177,12 +176,6 @@ public class HistoryOrder implements Serializable {
 
 	public void setVariable(String variable) {
 		this.variable = variable;
-	}
-	
-	public String getProcessName() {
-		Process process = ModelContainer.getEntity(this.processId);
-		if(process == null) return this.processId;
-		return process.getDisplayName();
 	}
 	
 	public String toString() {
