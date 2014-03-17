d6c3a5182043455ce2bff24be9ee5e74d53c7b4d
snakerflow <snakerflow@163.com>
2014-03-17 22:18:16 +0800

    增强:增加委托代理表的业务操作

---
+++ src/main/java/org/snaker/engine/IManagerService.java
@@ -0,0 +1,64 @@
+/* Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.snaker.engine;
+
+import java.util.List;
+
+import org.snaker.engine.access.Page;
+import org.snaker.engine.access.QueryFilter;
+import org.snaker.engine.entity.Surrogate;
+
+/**
+ * 管理服务接口,用于流程管理控制服务
+ * 委托管理
+ * 时限控制
+ * @author yuqs
+ * @since 1.4
+ */
+public interface IManagerService {
+	/**
+	 * 保存或更新委托代理对象
+	 * @param surrogate 委托代理对象
+	 */
+	public void saveOrUpdate(Surrogate surrogate);
+	
+	/**
+	 * 删除委托代理对象
+	 * @param id 委托代理主键id
+	 */
+	public void deleteSurrogate(String id);
+	
+	/**
+	 * 根据主键id查询委托代理对象
+	 * @param id 主键id
+	 * @return surrogate 委托代理对象
+	 */
+	public Surrogate getSurrogate(String id);
+	
+	/**
+	 * 根据过滤条件查询委托代理对象
+	 * @param filter 查询过滤器
+	 * @return List<Surrogate> 委托代理对象集合
+	 */
+	public List<Surrogate> getSurrogate(QueryFilter filter);
+	
+	/**
+	 * 根据过滤条件查询委托代理对象
+	 * @param page 分页对象
+	 * @param filter 查询过滤器
+	 * @return List<Surrogate> 委托代理对象集合
+	 */
+	public List<Surrogate> getSurrogate(Page<Surrogate> page, QueryFilter filter);
+}
---
+++ src/main/java/org/snaker/engine/core/ManagerService.java
@@ -0,0 +1,58 @@
+/* Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.snaker.engine.core;
+
+import java.util.List;
+
+import org.snaker.engine.IManagerService;
+import org.snaker.engine.access.Page;
+import org.snaker.engine.access.QueryFilter;
+import org.snaker.engine.entity.Surrogate;
+import org.snaker.engine.helper.AssertHelper;
+import org.snaker.engine.helper.StringHelper;
+
+/**
+ * @author yuqs
+ * @version 1.0
+ */
+public class ManagerService extends AccessService implements IManagerService {
+	public void saveOrUpdate(Surrogate surrogate) {
+		AssertHelper.notNull(surrogate);
+		if(StringHelper.isEmpty(surrogate.getId())) {
+			surrogate.setId(StringHelper.getPrimaryKey());
+			access().saveSurrogate(surrogate);
+		} else {
+			access().updateSurrogate(surrogate);
+		}
+	}
+
+	public void deleteSurrogate(String id) {
+		Surrogate surrogate = getSurrogate(id);
+		AssertHelper.notNull(surrogate);
+		access().deleteSurrogate(surrogate);
+	}
+
+	public Surrogate getSurrogate(String id) {
+		return getSurrogate(id);
+	}
+	
+	public List<Surrogate> getSurrogate(QueryFilter filter) {
+		return access().getSurrogate(null, filter);
+	}
+
+	public List<Surrogate> getSurrogate(Page<Surrogate> page, QueryFilter filter) {
+		return access().getSurrogate(page, filter);
+	}
+}
