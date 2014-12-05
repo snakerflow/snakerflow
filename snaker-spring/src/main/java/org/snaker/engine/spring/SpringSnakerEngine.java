23698beb4148309b132c87d2d99baac2a57f6274
snakerflow <snakerflow@163.com>
2014-12-05 22:55:36 +0800

    support properties injection

--- snaker-spring/src/main/java/org/snaker/engine/spring/SpringSnakerEngine.java
+++ snaker-spring/src/main/java/org/snaker/engine/spring/SpringSnakerEngine.java
@@ -20,20 +20,30 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import java.util.Properties;
+
 /**
  * spring环境使用的SnakerEngine实现类，主要接收spring的applicationContext对象
  * @author yuqs
  * @since 1.0
  */
-public class SpringSnakerEngine extends SnakerEngineImpl implements InitializingBean, ApplicationContextAware {
+public class SpringSnakerEngine extends SnakerEngineImpl
+        implements InitializingBean, ApplicationContextAware {
 	private ApplicationContext applicationContext;
+    private Properties properties;
 
 	public void afterPropertiesSet() throws Exception {
-		new SpringConfiguration(applicationContext).parser();
+        SpringConfiguration configuration = new SpringConfiguration(applicationContext);
+        if(properties != null) configuration.initProperties(properties);
+        configuration.parser();
 	}
 
 	public void setApplicationContext(ApplicationContext applicationContext)
 			throws BeansException {
 		this.applicationContext = applicationContext;
 	}
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
 }
